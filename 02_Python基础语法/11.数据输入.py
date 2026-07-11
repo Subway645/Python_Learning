d8c9a9ec2b000524426416c33022adf3a407d1cf
Subway645 <2559611066@qq.com>
2026-07-11 22:13:28 +0800

    基础语法

---
+++ 02_Python基础语法/11.数据输入.py
@@ -0,0 +1,11 @@
+print("请输入姓名")
+name = input()
+print(f"你的姓名是{name}")
+
+#input输入的是字符串
+print("请输入一串数字")
+num = input()
+print(f"这串数字的类型是{type(num)}")
+num = int(num)
+print(f"现在这串数字的类型是{type(num)}")
+
